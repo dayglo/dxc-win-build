12d505df8e87505c58f95ee54fefa304614a17e8
George Cairns <george.cairns@hpe.com>
2017-09-01 18:00:14 +0100

    nearly done, switching to mac foir debug

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 #!/usr/bin/env node
-
+var _ = require('lodash');
 var program = require('commander');
 var promisify = require('promisify-node');
 var fs = promisify("fs");
 
 program
 	.version('0.0.1')
+	.usage("<template name> [-o override]")
 	.arguments('<template>')
 	.action((template)=>{templateValue = template})
 	.option('-o, --override [type]', 'which template override to use [vcenter]', 'vcenter')
 	.parse(process.argv);
 
 var override = program.override;
+var overrideFolder = './base/fragments/' + override
 
 var packerTemplate;
 var overrides;
@@ -19,19 +21,37 @@ var overrides;
 console.log('Opening template ' + templateValue + '.json')
 if (override){console.log('  -Using override ' + override)}
 
-fs.open('./base/lib/packer-windows/' + templateValue + '.json' , 'r')
+fs.readFile('./base/lib/packer-windows/' + templateValue + '.json' )
 .then(JSON.parse)
 .then((packerTemplateJSON)=>{
 	packerTemplate = packerTemplateJSON
-	return fs.readdir('./base/fragments/' + override)
+	return fs.readdir(overrideFolder)
 })
 .then((fileList)=>{
-	return new Promise (
-		fileList.filter((file)=>{
+	return Promise.resolve(
+		fileList
+		.filter((file)=>{
 			var match = file.match(/\.json$/)
 			if (match) return match[0]
 		})
+		.map((file)=>{
+			var re = /^(.+?)(?:\.([^.]+))?$/;
+			return (re.exec(file)[1])
+		})
 	)
 })
+.then((overrideTypes)=>{
+
+	overrideTypes.forEach((overRideType)=>{
+		fs.readFile(overrideFolder + overRideType + '.json' )
+		.then((data)=>{
+			packerTemplate[overRideType] = data;
+			debugger
+		})
+	})
+
+	return packerTemplate
+})
+.then(JSON.stringify)
 .then(console.log)
 .catch(console.error)
