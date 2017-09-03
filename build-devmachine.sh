5796ea2b0cc8165ed87992634ef3f8f6c3833c97
George Cairns <george.cairns@hpe.com>
2017-09-04 00:16:55 +0100

    simple build script

---
+++ build-devmachine.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -ex
+outputfile=dxc-windows10-devmachine.json
+rm -f machinetypes/desktop/devmachine/$outputfile
+./mergetemplate.js base/vcenter-delta.json -o machinetypes/desktop/devmachine > machinetypes/desktop/devmachine/packer/$outputfile
+cd machinetypes/desktop/devmachine/packer
+packer build --var-file=../../../../variables.json $outputfile
